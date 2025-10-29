7d0f0f1cb41335909e5427305c25f2569f694bde
YashedBin <yash567pisudde@gmail.com>
2025-10-29 08:17:51 +0530

    Added MemoryProfiler.hpp
     Linux and Windows support

---
+++ cpp_core/headers/MemoryProfiler.hpp
@@ -0,0 +1,64 @@
+/* as Timer.hpp can run on any OS because of Chrono 
+Supporting it, Memory usage isnt the same
+So we have a Different Approach here Considering Linux 
+(I'm using WSL to program this whole project)
+and Windows to also get a Demo like feel*/
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#ifdef __linux__
+    // For Linux
+    #include <sys/resource.h>
+    #include <unistd.h>
+#elif _WIN32
+    // For Windows
+    #include <windows.h>
+    #include <psapi.h>
+#endif
+
+class MemoryProfiler {
+private:
+    std::string workDone;
+    bool emitJSON;
+    size_t startMemoryKB;
+    
+    // as I was using Windows to write and WSL to Compile I got two section in use
+    size_t getCurrentMemoryKB() {
+        #ifdef __linux__
+            struct rusage usage;
+            getrusage(RUSAGE_SELF, &usage);
+            return usage.ru_maxrss;
+        #elif _WIN32
+            PROCESS_MEMORY_COUNTERS_EX pmc;
+            GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+            return pmc.WorkingSetSize / 1024;
+        #else
+            return 0;
+        #endif
+    }
+    
+public:
+    MemoryProfiler(std::string work, bool json = false) 
+        : workDone(work), emitJSON(json) {
+        startMemoryKB = getCurrentMemoryKB();
+    }
+    
+    ~MemoryProfiler() {
+        size_t endMemoryKB = getCurrentMemoryKB();
+        long long deltaKB = endMemoryKB - startMemoryKB;
+        
+        if (emitJSON) {
+            std::cout << "{\"type\":\"memory\",\"work\":\"" << workDone 
+                      << "\",\"start_kb\":" << startMemoryKB
+                      << ",\"end_kb\":" << endMemoryKB
+                      << ",\"delta_kb\":" << deltaKB << "}\n";
+        } else {
+            std::cout << workDone << "\n"
+                      << "Memory Start: " << startMemoryKB << " KB\n"
+                      << "Memory End: " << endMemoryKB << " KB\n"
+                      << "Memory Delta: " << deltaKB << " KB\n";
+        }
+    }
+};
