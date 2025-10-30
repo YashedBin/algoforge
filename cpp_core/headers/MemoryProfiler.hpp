2591d863fe6c08703bf5e97feb882320fdc8cdcb
YashedBin <yash567pisudde@gmail.com>
2025-10-31 02:00:08 +0530

    WSL/Linux error for Memory usage calcualtion

--- cpp_core/headers/MemoryProfiler.hpp
+++ cpp_core/headers/MemoryProfiler.hpp
@@ -10,11 +10,10 @@ and Windows to also get a Demo like feel*/
 #include <string>
 
 #ifdef __linux__
-    // For Linux
     #include <sys/resource.h>
     #include <unistd.h>
+    #include <cstdio>
 #elif _WIN32
-    // For Windows
     #include <windows.h>
     #include <psapi.h>
 #endif
@@ -24,34 +23,39 @@ private:
     std::string workDone;
     bool emitJSON;
     size_t startMemoryKB;
-    
-    // as I was using Windows to write and WSL to Compile I got two section in use
+
     size_t getCurrentMemoryKB() {
-        #ifdef __linux__
-            struct rusage usage;
-            getrusage(RUSAGE_SELF, &usage);
-            return usage.ru_maxrss;
-        #elif _WIN32
-            PROCESS_MEMORY_COUNTERS_EX pmc;
-            GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
-            return pmc.WorkingSetSize / 1024;
-        #else
-            return 0;
-        #endif
+    #ifdef __linux__
+        // Read from /proc/self/statm for accurate RSS (resident set size)
+        long rss = 0L;
+        FILE* fp = fopen("/proc/self/statm", "r");
+        if (fp) {
+            if (fscanf(fp, "%*s%ld", &rss) != 1)
+                rss = 0L;
+            fclose(fp);
+        }
+        return (size_t)rss * (size_t)sysconf(_SC_PAGESIZE) / 1024; 
+    #elif _WIN32
+        PROCESS_MEMORY_COUNTERS_EX pmc;
+        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+        return pmc.WorkingSetSize / 1024;
+    #else
+        return 0;
+    #endif
     }
-    
+
 public:
-    MemoryProfiler(std::string work, bool json = false) 
+    MemoryProfiler(std::string work, bool json = false)
         : workDone(work), emitJSON(json) {
         startMemoryKB = getCurrentMemoryKB();
     }
-    
+
     ~MemoryProfiler() {
         size_t endMemoryKB = getCurrentMemoryKB();
-        long long deltaKB = endMemoryKB - startMemoryKB;
-        
+        long long deltaKB = static_cast<long long>(endMemoryKB) - static_cast<long long>(startMemoryKB);
+
         if (emitJSON) {
-            std::cout << "{\"type\":\"memory\",\"work\":\"" << workDone 
+            std::cout << "{\"type\":\"memory\",\"work\":\"" << workDone
                       << "\",\"start_kb\":" << startMemoryKB
                       << ",\"end_kb\":" << endMemoryKB
                       << ",\"delta_kb\":" << deltaKB << "}\n";
