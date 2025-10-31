64c3b2a1dcd489803ec4abc25bcc454bc1b680e2
YashedBin <yash567pisudde@gmail.com>
2025-10-31 22:19:52 +0530

    Final commit before merging

--- cpp_core/src/main.cpp
+++ cpp_core/src/main.cpp
@@ -1,8 +1,24 @@
-#include <MemoryProfiler.hpp>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include <Timer.hpp>
+#include <MemoryProfiler.hpp>
+
+int Kadane(std::vector<int>& arr){
+    Timer t("Kadane Core", true);
+    MemoryProfiler m("Kadane Core", true);
+
+    int max_sum = arr[0], curr = arr[0];
+    for(size_t i=1;i<arr.size();i++){
+        curr = std::max(arr[i], curr+arr[i]);
+        max_sum = std::max(max_sum, curr);
+    }
+    return max_sum;
+}
+
 int main(){
-Timer t("C++ Task", true);
-MemoryProfiler m("C++ Task", true);
-// Your code
-return 0;
+    std::vector<int> a = {-2,1,-3,4,-1,2,1,-5,4};
+    Timer t_all("Kadane Full Run", true);
+    MemoryProfiler m_all("Kadane Full Run", true);
+    std::cout << "Max Sum: " << Kadane(a) << "\n";
 }
